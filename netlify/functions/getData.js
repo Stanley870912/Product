c65b0953a7ac21ca63e14384062ddfb92ddb5d54
Stanley Lee <stanleylee@oits.com.tw>
2025-05-16 22:21:30 +0800

    測試

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,13 +1,11 @@
-
-const fs   = require('fs')
-const path = require('path')
-
+// netlify/functions/getData.js
 exports.handler = async () => {
-  try {
-    const filePath = path.join(__dirname, 'test.json')
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'))
-    return { statusCode: 200, body: JSON.stringify(data) }
-  } catch (err) {
-    return { statusCode: 500, body: JSON.stringify({ error: err.message }) }
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      message: "Hello from getData!",
+      timestamp: new Date().toISOString()
+    })
   }
 }
