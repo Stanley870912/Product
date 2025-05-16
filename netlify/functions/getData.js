6d5fc86be1590e8f14780f855d150b3b96d7dbc0
Stanley Lee <stanleylee@oits.com.tw>
2025-05-16 21:07:08 +0800

    測試

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,27 +1,6 @@
-// netlify/functions/getData.js
 exports.handler = async () => {
-  try {
-    let resp
-    try {
-      resp = await client.query(
-        q.Get(q.Ref(q.Collection('configs'), 'data'))
-      )
-    } catch (e) {
-      // 如果是 NotFound，就先建立一筆空的
-      if (e.requestResult.statusCode === 404) {
-        await client.query(
-          q.Create(
-            q.Ref(q.Collection('configs'), 'data'),
-            { data: { items: [] } }
-          )
-        )
-        resp = { data: { items: [] } }
-      } else {
-        throw e
-      }
-    }
-    return { statusCode: 200, body: JSON.stringify(resp.data) }
-  } catch (e) {
-    return { statusCode: 500, body: JSON.stringify({ message: e.message }) }
-  }
-}
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from Netlify Function" })
+  };
+};
